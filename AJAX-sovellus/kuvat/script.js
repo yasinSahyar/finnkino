401f3e268eec0aaaef55c9a5cecc50c051474346
urumci <nijatt23@gmail.com>
2023-11-28 12:05:35 +0200

    11-28 only

--- AJAX-sovellus/kuvat/script.js
+++ AJAX-sovellus/kuvat/script.js
@@ -1,3 +1,5 @@
+// script.js
+
 const theaterSelect = document.getElementById("theater-select");
 const timeSelect = document.getElementById("time-select");
 const movieList = document.getElementById("laatikko");
@@ -9,33 +11,46 @@ function fetchData(selectedTheater, selectedDate) {
         .then(response => response.text())
         .then(str => new DOMParser().parseFromString(str, "text/xml"))
         .then(data => {
-            movieList.innerHTML = ""; 
+            movieList.innerHTML = "";
 
             const elokuvat = data.querySelectorAll('Show');
             for (let i = 0; i < elokuvat.length; i++) {
                 let show = elokuvat[i];
-                let title = show.getElementsByTagName('Title')[0].innerHTML;
-                let genres = show.getElementsByTagName('Genres')[0].innerHTML;
-                let eventSmallImagePortrait = show.getElementsByTagName('EventSmallImagePortrait')[0].innerHTML;
-                let dtLocalRelease = show.getElementsByTagName('dtLocalRelease')[0].innerHTML;
-                let director = ""; 
-                let actors = "";   
-                let duration = show.getElementsByTagName('LengthInMinutes')[0].innerHTML;
+                let showDate = new Date(show.getElementsByTagName('dttmShowStart')[0].innerHTML);
 
-                let elokuvaContainer = document.createElement('div');
-                elokuvaContainer.classList.add('elokuva-container');
-                let html = `
-                    <img src="${eventSmallImagePortrait}" alt="${title}">
-                    <div class="elokuva-tiedot">
-                        <h1>${title}</h1>
-                        <p>Screening time: ${formatDate(dtLocalRelease)}</p>
-                        <p>Genre: ${genres}</p>
-                        <p>Director: ${director}</p>
-                        <p>Actors: ${actors}</p>
-                        <p>Duration: ${duration} min</p>
-                    </div>`;
-                elokuvaContainer.innerHTML = html;
-                movieList.appendChild(elokuvaContainer);
+                // Extract year, month, and day from the show date
+                let showYear = showDate.getFullYear();
+                let showMonth = showDate.getMonth() + 1; // Month is zero-based
+                let showDay = showDate.getDate();
+
+                // Extract year, month, and day from the selected date
+                let [selectedYear, selectedMonth, selectedDay] = selectedDate.split('-').map(Number);
+
+                // Check if the movie date matches the selected date (ignoring time)
+                if (showYear === selectedYear && showMonth === selectedMonth && showDay === selectedDay) {
+                    let title = show.getElementsByTagName('Title')[0].innerHTML;
+                    let genres = show.getElementsByTagName('Genres')[0].innerHTML;
+                    let eventSmallImagePortrait = show.getElementsByTagName('EventSmallImagePortrait')[0].innerHTML;
+                    let dtLocalRelease = show.getElementsByTagName('dttmShowStart')[0].innerHTML;
+                    let director = ""; 
+                    let actors = "";   
+                    let duration = show.getElementsByTagName('LengthInMinutes')[0].innerHTML;
+
+                    let elokuvaContainer = document.createElement('div');
+                    elokuvaContainer.classList.add('elokuva-container');
+                    let html = `
+                        <img src="${eventSmallImagePortrait}" alt="${title}">
+                        <div class="elokuva-tiedot">
+                            <h1>${title}</h1>
+                            <p>Screening time: ${formatDate(dtLocalRelease)}</p>
+                            <p>Genre: ${genres}</p>
+                            <p>Director: ${director}</p>
+                            <p>Actors: ${actors}</p>
+                            <p>Duration: ${duration} min</p>
+                        </div>`;
+                    elokuvaContainer.innerHTML = html;
+                    movieList.appendChild(elokuvaContainer);
+                }
             }
         })
         .catch(error => {
@@ -46,7 +61,7 @@ function fetchData(selectedTheater, selectedDate) {
 function updateMovies() {
     const selectedTheater = theaterSelect.value;
     const selectedDate = timeSelect.value;
-
+    console.log(selectedDate); // Add this line to check the selected date value
     fetchData(selectedTheater, selectedDate);
 }
 
@@ -59,18 +74,8 @@ theaterSelect.addEventListener("change", updateMovies);
 timeSelect.addEventListener("change", updateMovies);
 
 
-const startDate = new Date('2023-11-26');
-const endDate = new Date('2023-12-26');
-let currentDate = startDate;
-
-while (currentDate <= endDate) {
-    const formattedDate = currentDate.toISOString().split('T')[0];
-    const option = document.createElement('option');
-    option.value = formattedDate;
-    option.text = formatDate(formattedDate);
-    timeSelect.add(option);
-    currentDate.setDate(currentDate.getDate() + 1);
-}
 
 
+
+// Initialize with the first option values
 updateMovies();
